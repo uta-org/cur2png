c36a7d65e46123278c3851dca1a6127bc55b0879
Ikillnukes <ikillnukes1@gmail.com>
2019-06-05 04:52:50 +0200

    Added docs; added CLI

--- cur2png/Program.cs
+++ cur2png/Program.cs
@@ -1,23 +1,66 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
-using System.Reflection;
 using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using CommandLine;
 
 namespace cur2png
 {
+    /// <summary>
+    /// The Program class
+    /// </summary>
     internal class Program
     {
+        /// <summary>
+        /// The Options class (used for CLI)
+        /// </summary>
+        public class Options
+        {
+            /// <summary>
+            /// Gets or sets the target folder.
+            /// </summary>
+            /// <value>
+            /// The target folder.
+            /// </value>
+            [Option('f', "folder", Required = true, HelpText = "Set target folder for .")]
+            public string TargetFolder { get; set; }
+        }
+
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
         private static void Main(string[] args)
         {
-            string folder = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "curs");
-            string outFolder = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "png");
+            Parser.Default.ParseArguments<Options>(args)
+                .WithParsed(o =>
+                {
+                    if (string.IsNullOrEmpty(o.TargetFolder))
+                    {
+                        throw new Exception("Invalid target folder specified.\nPress any key to exit...");
+                    }
+                    else
+                    {
+                        Execute(o.TargetFolder);
+                    }
+                });
+
+            Console.Read();
+        }
+
+        /// <summary>
+        /// Executes the app to process the specified folder.
+        /// </summary>
+        /// <param name="folder">The folder.</param>
+        private static void Execute(string folder)
+        {
+            // string folder = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "curs");
+
+            // Create folder next to the target folder
+            string outFolder = Path.Combine(Path.GetDirectoryName(folder), "png");
 
             if (!Directory.Exists(outFolder))
                 Directory.CreateDirectory(outFolder);
@@ -46,7 +89,7 @@ namespace cur2png
                 //Using LoadCursorFromFile from user32.dll, get a handle to the icon
                 IntPtr hCursor = LoadCursorFromFile(file);
 
-                // "C:\\Windows\\Cursors\\Windows Aero\\aero_busy.ani"
+                // Example of folder: "C:\\Windows\\Cursors\\Windows Aero\\aero_busy.ani"
 
                 //Create a Cursor object from that handle
                 Cursor cursor = new Cursor(hCursor);
@@ -69,10 +112,13 @@ namespace cur2png
                 }
             }
 
-            Console.WriteLine($"Succesfully created {files.Count() - errorCount} files! (Errors: {errorCount}) Press any key to exit...");
-            Console.Read();
+            Console.WriteLine(
+                $"Succesfully created {files.Count() - errorCount} files! (Errors: {errorCount}) Press any key to exit...");
         }
 
+        /// <summary>
+        /// The ICONINFO folder
+        /// </summary>
         [StructLayout(LayoutKind.Sequential)]
         private struct ICONINFO
         {
@@ -83,15 +129,36 @@ namespace cur2png
             public IntPtr hbmColor;
         }
 
+        /// <summary>
+        /// Gets the icon information.
+        /// </summary>
+        /// <param name="hIcon">The h icon.</param>
+        /// <param name="pIconInfo">The p icon information.</param>
+        /// <returns></returns>
         [DllImport("user32")]
         private static extern bool GetIconInfo(IntPtr hIcon, out ICONINFO pIconInfo);
 
+        /// <summary>
+        /// Loads the cursor from file.
+        /// </summary>
+        /// <param name="lpFileName">Name of the lp file.</param>
+        /// <returns></returns>
         [DllImport("user32.dll")]
         private static extern IntPtr LoadCursorFromFile(string lpFileName);
 
+        /// <summary>
+        /// Deletes the object.
+        /// </summary>
+        /// <param name="hObject">The h object.</param>
+        /// <returns></returns>
         [DllImport("gdi32.dll", SetLastError = true)]
         private static extern bool DeleteObject(IntPtr hObject);
 
+        /// <summary>
+        /// Creates a bitmap from cursor.
+        /// </summary>
+        /// <param name="cur">The current.</param>
+        /// <returns></returns>
         private static Bitmap BitmapFromCursor(Cursor cur)
         {
             ICONINFO ii;
